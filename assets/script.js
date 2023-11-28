560e75aae39d11f4096a78bd1fc24454667c3495
seandonnelly92 <sean_donnelly@hotmail.co.uk>
2023-11-28 13:51:29 +0000

    Declaring and calling JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,38 @@
-var topic = "HTML";
+var topics = ["HTML", "CSS", "Git", "JavaScript"]; 
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
+function listTopics() {
+       for(var x = 0; x < topics.length; x++) {
 
-if (topic === 'HTML') {
+        console.log(topics[x])
+    }
+}
+
+function selectTopic() {
+
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
  
- } else if (topic === 'CSS') {
+ } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
 
- } else if (topic === 'Git') {
+ } else if (randomTopic === 'Git') {
     console.log("Let's study Git!"); 
 
- } else if (topic === 'JavaScript') {
+ } else if (randomTopic === 'JavaScript') {
     console.log("Let's study JavaScript");
  
 } else {
     
     console.log("Please try again!");
  }
-
-
-*/
-
-var topics = ["HTML", "CSS", "Git", "JavaScript"]; 
-
-for(var x = 0; x < topics.length; x++) {
-
-    console.log(topics[x])
 }
+
+console.log("Here are the list of topics we learned through Prework:")
+listTopics()
+console.log("Which topic should we study now?");
+selectTopic()
+
+
+
+
